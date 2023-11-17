facab53e51c148165b006ba8fb050ed0c3c79f11
CsongetoCsongor <csongeto.csongor@students.jedlik.eu>
2023-11-17 12:49:01 +0100

    kezdődik

---
+++ PVK_MAIN/PVK_MAIN/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace PVK_MAIN
+{
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+            int jozansag = 100;
+
+            int eletkedv = 50;
+
+            int verszomj = 50;
+
+            int penz = 0;
+
+            bool buksza_birtoklasa = false;
+
+            bool hajokulcs_birtoklasa = false;
+
+            bool tengeri_gyongy_birtoklasa = false;
+
+            bool volt_e_verekedes_kocsmaban = false;
+
+            bool vadaszhaz_meglatogatva = false;
+
+            bool fura_kulcs_birtoklasa = false;
+
+            bool tarto_oszlop_eltova = false;
+
+            bool jegygyuru_birtoklasa = false;
+
+            bool karpatok_meglatogatva = false;
+
+            bool aranykulcs_birtoklasa = false;
+
+            bool bankrablas_megtortent = false;
+
+            bool szent_korona_birtoklasa = false;
+
+            bool rab_megrugva = false;
+
+            string camefrom_message = "";
+        }
+    }
+}
