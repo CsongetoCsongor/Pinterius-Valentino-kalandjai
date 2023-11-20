383d2d93716f50f8f91f9f11c8f84cacb7c914ce
CsongetoCsongor <csongeto.csongor@students.jedlik.eu>
2023-11-20 09:47:21 +0100

    változók public static

--- PVK_MAIN/PVK_MAIN/Program.cs
+++ PVK_MAIN/PVK_MAIN/Program.cs
@@ -2,43 +2,47 @@
 {
     internal partial class Program
     {
+
+
+        public static int jozansag = 100;
+
+        public static int eletkedv = 50;
+
+        public static int verszomj = 50;
+
+        public static int penz = 0;
+
+        public static bool buksza_birtoklasa = false;
+
+        public static bool hajokulcs_birtoklasa = false;
+
+        public static bool tengeri_gyongy_birtoklasa = false;
+
+        public static bool volt_e_verekedes_kocsmaban = false;
+
+        public static bool vadaszhaz_meglatogatva = false;
+
+        public static bool fura_kulcs_birtoklasa = false;
+
+        public static bool tarto_oszlop_eltova = false;
+
+        public static bool jegygyuru_birtoklasa = false;
+
+        public static bool karpatok_meglatogatva = false;
+
+        public static bool aranykulcs_birtoklasa = false;
+
+        public static bool bankrablas_megtortent = false;
+
+        public static bool szent_korona_birtoklasa = false;
+
+        public static bool rab_megrugva = false;
+
+        public static string camefrom_message = "";
+
         static void Main(string[] args)
         {
-            int jozansag = 100;
-
-            int eletkedv = 50;
-
-            int verszomj = 50;
-
-            int penz = 0;
-
-            bool buksza_birtoklasa = false;
-
-            bool hajokulcs_birtoklasa = false;
-
-            bool tengeri_gyongy_birtoklasa = false;
-
-            bool volt_e_verekedes_kocsmaban = false;
-
-            bool vadaszhaz_meglatogatva = false;
-
-            bool fura_kulcs_birtoklasa = false;
-
-            bool tarto_oszlop_eltova = false;
-
-            bool jegygyuru_birtoklasa = false;
-
-            bool karpatok_meglatogatva = false;
-
-            bool aranykulcs_birtoklasa = false;
-
-            bool bankrablas_megtortent = false;
-
-            bool szent_korona_birtoklasa = false;
-
-            bool rab_megrugva = false;
-
-            string camefrom_message = "";
+            
         }
     }
 }
