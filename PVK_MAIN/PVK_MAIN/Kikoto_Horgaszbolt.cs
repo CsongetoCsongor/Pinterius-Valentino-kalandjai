c6c9d7f5c8443ab713f312486c8d52e902781b88
Bartha Bende <bartha.bende@students.jedlik.eu>
2023-11-24 12:20:08 +0100

    Horgaszbolt, kikoto elkezdve

--- PVK_MAIN/PVK_MAIN/Kikoto_Horgaszbolt.cs
+++ PVK_MAIN/PVK_MAIN/Kikoto_Horgaszbolt.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace PVK_MAIN
 {
-    internal partial class Kikoto_Horgaszbolt
+    internal partial class Program
     {
         static void Kikoto()
         {
