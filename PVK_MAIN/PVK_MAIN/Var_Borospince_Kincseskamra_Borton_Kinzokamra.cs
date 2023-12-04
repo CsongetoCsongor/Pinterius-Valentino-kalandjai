9440d4fc1ae2e3b07ca2983e8d67cdc8b360658b
Csongor Csöngető <csongeto.csongor@students.jedlik.eu>
2023-12-04 20:52:39 +0100

    VÁÁÁÁÁ

--- PVK_MAIN/PVK_MAIN/Var_Borospince_Kincseskamra_Borton_Kinzokamra.cs
+++ PVK_MAIN/PVK_MAIN/Var_Borospince_Kincseskamra_Borton_Kinzokamra.cs
@@ -10,8 +10,41 @@ namespace PVK_MAIN
     {
         static void Var()
         {
+            Console.Clear();
+            helyszin = "Világuralom Vár";
+            IrjaKiAStatokat("Engedély nélkül bementél a várba, de az őrök nem mertek szólni, mert úgy néztél ki, mint az Antikrisztus.");
 
-        }
+            Console.WriteLine();
+            Console.WriteLine("Lehetőségek:");
+            Console.WriteLine("\t1. Lemész a borospincébe");
+            Console.WriteLine("\t2. Bemész a börtönbe");
+            Console.WriteLine("\t3. Kimész a főútra");
+            int input = BekerLehetosegek(3);
+
+            if (input == 1)
+            {
+                Console.WriteLine("Lemész a borospincébe");
+                elozo_hely_uzenete = "";
+                elozo_helyszin = "Vár";
+                Console.ReadKey();
+                Borospince();
+            }
+            else if(input == 2)
+            {
+                Console.WriteLine("Bemész a börtönbe...");
+                elozo_hely_uzenete = "";
+                elozo_helyszin = "Vár";
+                Console.ReadKey();
+                Borton();
+            }
+            else if(input == 3)
+            {
+                Console.WriteLine("Kimész a főútra...");
+                elozo_hely_uzenete = "";
+                elozo_helyszin = "Vár";
+                Console.ReadKey();
+                Fout();
+            }
 
         static void Borospince()
         {
