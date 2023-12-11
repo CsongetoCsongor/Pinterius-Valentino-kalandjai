5139a9bfe0a810b86ec94f89e89c2beab9d5a731
CsongetoCsongor <csongeto.csongor@students.jedlik.eu>
2023-12-11 10:25:51 +0100

    Detox

--- PVK_MAIN/PVK_MAIN/Detox_Borton.cs
+++ PVK_MAIN/PVK_MAIN/Detox_Borton.cs
@@ -14,23 +14,98 @@ namespace PVK_MAIN
             helyszin = "Második Esély Klinika, Detoxikáló részleg";
             IrjaKiAStatokat("A véralkoholszinted túl magas volt, kiütötted magad. Szerencsédre rádtaláltak, és bevittek a detoxba.");
 
-            
-
+            Console.WriteLine("Kérdésekre kell válaszolnod, hogy leteszteljék, hogy elég józan vagy-e.");
             Console.WriteLine();
-            Console.WriteLine("Lehetőségek:");
-            Console.WriteLine("\t1. Lemész a borospincébe");
-            Console.WriteLine("\t2. Bemész a börtönbe");
-            Console.WriteLine("\t3. Kimész a főútra");
-            int input = BekerLehetosegek(3);
+            Console.WriteLine("Vigyáz! Ha 2-nál többet hibázol megölnek, mert úgy ítélik, nem vagy hasznos tagja a társadalomnak.");
+            Console.WriteLine();
 
-            if (input == 1)
+            Random esely = new Random();
+
+            int mistakes = 0;
+
+            int rounds = 0;
+
+            void feladat1()
             {
-                Console.WriteLine("Lemész a borospincébe...");
-                elozo_hely_uzenete = "";
-                elozo_helyszin = "Világuralom Vár";
-                Console.ReadKey();
-                Borospince();
+                Console.Clear();
+                int a = esely.Next(1, 16);
+                int b = esely.Next(1, 16);
+
+                Console.WriteLine($"Mennyi {a} + {b}?");
+                int valasz;
+
+                do
+                {
+                    Console.Write("Válasz: ");
+
+                }
+                while (!int.TryParse(Console.ReadLine(), out valasz));
+
+                if (valasz == a + b)
+                {
+                    Console.WriteLine("Helyes válasz!");
+                    rounds++;
+                }
+                else
+                {
+                    Console.WriteLine("Hibát vétettünk!!!");
+                    rounds++;
+                    mistakes++;
+                }
+                
             }
+
+            void feladat2()
+            {
+                Console.Clear();
+                int a = esely.Next(1, 16);
+                int b = esely.Next(1, 16);
+
+                Console.WriteLine($"Mennyi {a} - {b}?");
+                int valasz;
+
+                do
+                {
+                    Console.Write("Válasz: ");
+
+                }
+                while (!int.TryParse(Console.ReadLine(), out valasz));
+
+                if (valasz == a - b)
+                {
+                    Console.WriteLine("Helyes válasz!");
+                    rounds++;
+                }
+                else
+                {
+                    Console.WriteLine("Hibát vétettünk!!!");
+                    rounds++;
+                    mistakes++;
+                }
+
+            }
+
+            while (rounds <= 5 && mistakes <3)
+            {
+                if (rounds % 2 == 0)
+                {
+                    feladat1();
+                }
+                else
+                {
+                    feladat2();
+                }
+            }
+            if(mistakes == 3)
+            {
+                Console.WriteLine("Meghaltál!");
+            }
+            else
+            {
+                Console.WriteLine("Kijutottál!");
+            }
+
+            
         }
     }
 }
