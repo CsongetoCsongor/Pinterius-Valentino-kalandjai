90ce6dfe83ed4ca35978a2718b9bc44e6c555ce7
[Pap László] <[pap.laszlo@students.jedlik.eu]>
2023-12-11 10:09:17 +0100

    purganyurga

---
+++ PVK_MAIN/PVK_MAIN/Purgatorium.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PVK_MAIN
+{
+    internal partial class Program
+    {
+        static void Purgatorium()
+        {
+            Console.Clear();
+            helyszin = "Purgatórium";
+            IrjaKiAStatokat("A tisztítótűzben találod magad, ahol ítéletet kérhetsz az Úrtól");
+            Console.WriteLine();
+
+            Console.WriteLine("Lehetőségek:");
+            Console.WriteLine("\t");
+            Console.WriteLine("\t");
+            Console.WriteLine("\t");
+            Console.WriteLine("\t");
+            Console.WriteLine("\t");
+            Console.WriteLine("\t");
+            TODO
+
+            int x = BekerLehetosegek();
+
+            if (x == )
+            {
+
+            }
+
+            else if (x == )
+            {
+
+            }
+
+
+        }
+    }
+}
